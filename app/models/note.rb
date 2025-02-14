183f4795c5f8997dfa03b920a810466b770ec244
ValentinPg <valentinpugliese@outlook.com>
2025-02-14 09:48:22 -0300

    Ya se pueden crear las notas y quedan asociadas a los usuario

--- app/models/note.rb
+++ app/models/note.rb
@@ -11,8 +11,9 @@
 #  updated_at :datetime         not null
 #
 class Note < ApplicationRecord
-  belongs_to :users
+  belongs_to :user
 
-  validates :note_type, inclusion: {in: %w(review critique)},
-  message: "El atributo note_type tiene que ser 'review' o 'critique'"
+  validates :note_type, inclusion: {in: %w(review critique),
+  message: "El atributo note_type tiene que ser 'review' o 'critique'"}
+  validates :title, :content, presence: true
 end
