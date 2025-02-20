1ee9f8b5458879a37bddc8ab73078e022f8c0e58
ValentinPg <valentinpugliese@outlook.com>
2025-02-20 14:34:35 -0300

    Revert "Se limitan los endpoints del NoteController a index y show"
    This reverts commit cc3a764badac4ef2795f993ac44cf6497ef920da.
    
    Se eliminan comits que no son parte de esta card, van en el proximo PR

--- app/controllers/api/v1/notes_controller.rb
+++ app/controllers/api/v1/notes_controller.rb
@@ -1,13 +1,8 @@
-module Api
-  module V1
-    class NotesController < ApplicationController
-      def index
-        @note_type = params[:type]
-        @notes = Note.all
-        render json: @notes
-      end
-
-      def show; end
-    end
+class NotesController < ApplicationController
+  def index
+    @notes = Note.all
+    render json: @notes
   end
+
+  def show; end
 end
