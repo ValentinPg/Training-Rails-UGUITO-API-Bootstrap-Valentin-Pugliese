422ac76ba52ebbabfb3cbd45049be457a5b33a14
ValentinPg <valentinpugliese@outlook.com>
2025-02-20 14:37:41 -0300

    Se scan cambios que pertenecen a la branch TR-726

--- config/routes.rb
+++ config/routes.rb
@@ -20,7 +20,6 @@ Rails.application.routes.draw do
     end
   end
 
-  get '/notes', to: 'notes#index'
   get '/async_request/jobs/:id', to: 'async_request/jobs#show'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
