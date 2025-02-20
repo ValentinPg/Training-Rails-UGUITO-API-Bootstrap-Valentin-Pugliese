c851b4980db586e6cce8b22e02a217d6a1e998c2
ValentinPg <valentinpugliese@outlook.com>
2025-02-20 17:42:10 -0300

    Index acepta todos los parametros

--- app/controllers/api/v1/notes_controller.rb
+++ app/controllers/api/v1/notes_controller.rb
@@ -1,6 +1,8 @@
 module Api
   module V1
     class NotesController < ApplicationController
+      before_action :validate_params
+
       def index
         render json: filtered_notes, status: :ok, each_serializer: NoteSerializer
       end
@@ -10,7 +12,13 @@ module Api
       private
 
       def filtered_notes
-        Note.where(note_type: params[:type]).order(:created_at).page(params[:page]).per(params[:page_size])
+        Note.where(note_type: params[:type]).order(created_at: params[:order]).page(params[:page]).per(params[:page_size])
+      end
+
+      def validate_params
+        valid_types = %w[review critique]
+        return if valid_types.include?(params[:type])
+        render json: { error: 'Invalid type' }, status: :bad_request
       end
     end
   end
