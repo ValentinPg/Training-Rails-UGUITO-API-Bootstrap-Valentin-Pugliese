422115f572ea2604cd0c1d64684e16c30953cb0a
ValentinPg <valentinpugliese@outlook.com>
2025-02-21 15:37:06 -0300

    Se arregla el routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,8 @@ Rails.application.routes.draw do
         get :async, to: 'books#index_async'
       end
     end
+
+      resources :notes, only: [:index, :show]
   end
 
   get '/async_request/jobs/:id', to: 'async_request/jobs#show'
