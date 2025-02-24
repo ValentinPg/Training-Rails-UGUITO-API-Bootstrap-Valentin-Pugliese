66e79d3167b458a2e0f16ef9c8d4e228edc831a9
ValentinPg <valentinpugliese@outlook.com>
2025-02-24 16:57:56 -0300

    tests de review y critique

--- spec/controllers/api/v1/notes_controller_spec.rb
+++ spec/controllers/api/v1/notes_controller_spec.rb
@@ -1,13 +1,39 @@
 require 'rails_helper'
 
-describe API::V1::NotesController, type: :controller do
+describe Api::V1::NotesController, type: :controller do
   describe 'GET #index' do
+    let(:review) { create(:note, note_type: 'review', user: user) }
+    let(:critique) { create(:note, note_type: 'critique', user: user) }
+    # let!(:expected) do
+    #     ActiveModel::Serializer::CollectionSerializer.new(notes_expected,
+    #                                                       serializer: IndexNoteSerializer).to_json
+    #   end
+
     context 'when there is a user logged in' do
       include_context 'with authenticated user'
-      context 'when fetching critiques' do
-      end
+
+      let!(:expected) { IndexNoteSerializer.new(notes_expected).to_json }
 
       context 'when fetching reviews' do
+        before { get :index, params: { type: 'review', order: 'asc', page: 1, page_size: 1 } }
+
+        let(:notes_expected) { review }
+
+        it do
+          expect(response_body.to_json).to eq(expected)
+        end
+
+        it { expect(response).to have_http_status(:ok) }
+      end
+
+      context 'when fetching critiques' do
+        before { get :index, params: { type: 'critique', order: 'asc', page: 1, page_size: 1 } }
+
+        let(:notes_expected) { critique }
+
+        it { expect(response_body.to_json).to eq(expected) }
+
+        it { expect(response).to have_http_status(:ok) }
       end
 
       context 'when ordering asc' do
@@ -17,6 +43,11 @@ describe API::V1::NotesController, type: :controller do
       end
 
       context 'when passing invalid parameters' do
+        before { get :index, params: {} }
+
+        let(:notes_expected) { critique }
+
+        it { expect(response).to have_http_status(:bad_request) }
       end
 
       context 'when passing page_size and page' do
@@ -24,12 +55,9 @@ describe API::V1::NotesController, type: :controller do
     end
 
     context 'when there is not a user logged in' do
-    end
-  end
+      before { get :index }
 
-  describe 'GET #show' do
-    context 'when there is a user logged in' do
-      include_context 'with authenticated user'
+      it_behaves_like 'unauthorized'
     end
   end
 end
