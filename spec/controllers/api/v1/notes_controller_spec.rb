4173eb12ec2edb100e3047a250cc82461a92e3e8
ValentinPg <valentinpugliese@outlook.com>
2025-02-27 15:16:30 -0300

    feat: refactor tests

--- spec/controllers/api/v1/notes_controller_spec.rb
+++ spec/controllers/api/v1/notes_controller_spec.rb
@@ -2,51 +2,87 @@ require 'rails_helper'
 
 describe Api::V1::NotesController, type: :controller do
   describe 'GET #index' do
-    let(:review) { create_list(:note, 3, note_type: 'review', user: user) }
-    let(:critique) { create_list(:note, 3, note_type: 'critique', user: user) }
 
     context 'when there is a user logged in' do
       include_context 'with authenticated user'
 
-      let!(:expected) do
-        ActiveModel::Serializer::CollectionSerializer.new(notes_expected,
-                                                          serializer: IndexNoteSerializer).to_json
-      end
+      context 'when fetching data' do
+        let!(:expected) do
+          ActiveModel::Serializer::CollectionSerializer.new(notes,
+                                                            serializer: IndexNoteSerializer).to_json
+        end
 
-      context 'when fetching reviews' do
-        before { get :index, params: { type: 'review', order: 'asc' } }
+        context 'when fetching reviews' do
+          before do
+            notes
+            get :index, params: { type: 'review' }
+          end
 
-        let(:notes_expected) { review }
+          let(:notes) { create_list(:note, 3, note_type: 'review', user: user) }
 
-        it { expect(response_body.to_json).to eq(expected) }
+          it { expect(response_body.to_json).to eq(expected) }
 
-        it { expect(response).to have_http_status(:ok) }
-      end
+          it { expect(response).to have_http_status(:ok) }
+        end
 
-      context 'when fetching critiques' do
-        before { get :index, params: { type: 'critique', order: 'asc' } }
+        context 'when fetching critiques' do
+          before do
+            notes
+            get :index, params: { type: 'critique' }
+          end
 
-        let(:notes_expected) { critique }
+          let(:notes) { create_list(:note, 3, note_type: 'critique', user: user) }
 
-        it { expect(response_body.to_json).to eq(expected) }
+          it { expect(response_body.to_json).to eq(expected) }
 
-        it { expect(response).to have_http_status(:ok) }
+          it { expect(response).to have_http_status(:ok) }
+        end
+
+        context 'when passing page_size and page' do
+          let(:page) { 1 }
+          let(:page_size) { 2 }
+          let(:notes) { create_list(:note, 2, note_type: 'critique', user: user) }
+
+          before { get :index, params: { type: 'critique', page: page, page_size: page_size } }
+
+          it { expect(response_body.to_json).to eq(expected) }
+        end
+
+        context 'when checking serializer attributes' do
+          before { get :index, params: { type: 'review' } }
+
+          let(:notes) { create_list(:note, 3, 'review', user: user) }
+          let(:body) { JSON.parse(expected) }
+
+          %w[id title type content_length].each do |attribute|
+            it { expect(body.first.keys).to include(attribute) }
+          end
+        end
       end
 
       context 'when ordering results' do
-        let(:notes_expected) { critique }
         let(:random_item) { rand(0..(response_body.length - 2)) }
         let(:first_note) { Note.find(response_body[random_item]['id']) }
         let(:last_note) { Note.find(response_body[random_item + 1]['id']) }
 
-        context 'with order asc' do
-          before { get :index, params: { type: 'critique', order: 'asc' } }
+        let(:notes) { create_list(:note, 20, user: user) }
 
-          it { expect(first_note.created_at).to be <= last_note.created_at }
+        context 'with order asc' do
+          before do
+            notes
+            get :index, params: { type: 'critique', order: 'asc' }
+          end
+
+          it {
+            expect(first_note.created_at).to be <= last_note.created_at
+          }
         end
 
         context 'with order desc' do
-          before { get :index, params: { type: 'critique', order: 'desc' } }
+          before do
+            notes
+            get :index, params: { type: 'critique', order: 'desc' }
+          end
 
           it { expect(first_note.created_at).to be >= last_note.created_at }
         end
@@ -55,30 +91,7 @@ describe Api::V1::NotesController, type: :controller do
       context 'when passing invalid parameters' do
         before { get :index, params: { type: 'test' } }
 
-        let(:notes_expected) { critique }
-
         it { expect(response).to have_http_status(:bad_request) }
-      end
-
-      context 'when passing page_size and page' do
-        let(:page) { 1 }
-        let(:page_size) { 2 }
-        let(:notes_expected) { critique.first(2) }
-
-        before { get :index, params: { type: 'critique', order: 'asc', page: page, page_size: page_size } }
-
-        it { expect(response_body.to_json).to eq(expected) }
-      end
-
-      context 'when checking serializer attributes' do
-        before { get :index, params: { type: 'review' } }
-
-        let(:notes_expected) { review }
-        let(:body) { JSON.parse(expected) }
-
-        %w[id title type content_length].each do |attribute|
-          it { expect(body.first.keys).to include(attribute) }
-        end
       end
     end
 
