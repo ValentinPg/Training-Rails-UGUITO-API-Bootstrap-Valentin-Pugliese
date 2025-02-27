1bfc6d052d1d5d6f0e47f5f85bc439b403086f84
ValentinPg <valentinpugliese@outlook.com>
2025-02-27 16:41:11 -0300

    feat: generalizacion paging.rb

--- spec/support/shared_examples/paging.rb
+++ spec/support/shared_examples/paging.rb
@@ -1,11 +1,13 @@
 shared_examples 'test pagination' do
+  let(:expected) { test_subject.page(page).per(page_size) }
+
   it 'returns the correct number of items' do
-    expect(response_body.size).to eq(page_size)
+    expect(response_body.size).to eq(expected.size)
   end
 
   it 'returns te correct items' do
-    expected_ids = test_subject.page(page).per(page_size).map { |i| i.id }
-    response_ids = response_body.map { |i| i['id'] }
+    expected_ids = expected.map { |i| i.id }.to_set
+    response_ids = response_body.map { |i| i['id'] }.to_set
     expect(response_ids).to eq(expected_ids)
   end
 end
