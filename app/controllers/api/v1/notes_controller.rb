2011815d9f7f6a63f503bb2f624095bb801feb34
ValentinPg <valentinpugliese@outlook.com>
2025-03-06 11:58:28 -0300

    feat: cambio de nombre de serializer y de metodos

--- app/controllers/api/v1/notes_controller.rb
+++ app/controllers/api/v1/notes_controller.rb
@@ -2,8 +2,8 @@ module Api
   module V1
     class NotesController < ApplicationController
       def index
-        return render_error unless validate_type
-        render json: paged_notes, status: :ok, each_serializer: IndexNoteSerializer
+        return render_error unless valid_type?
+        render json: paged_notes, status: :ok, each_serializer: NoteBriefSerializer
       end
 
       def show
@@ -13,7 +13,7 @@ module Api
       private
 
       def ordered_notes
-        validate_order ? notes.order(created_at: params[:order]) : notes
+        should_order_notes? ? notes.order(created_at: params[:order]) : notes
       end
 
       def paged_notes
@@ -28,7 +28,7 @@ module Api
         Note.find(params[:id])
       end
 
-      def validate_order
+      def should_order_notes?
         params[:order].present?
       end
 
@@ -37,7 +37,7 @@ module Api
                status: :bad_request
       end
 
-      def validate_type
+      def valid_type?
         Note.note_types.include?(params[:type])
       end
     end
--- app/serializers/index_note_serializer.rb
+++ app/serializers/index_note_serializer.rb
@@ -1,7 +1,5 @@
-class IndexNoteSerializer < ActiveModel::Serializer
+class NoteBriefSerializer < ActiveModel::Serializer
   attributes :id, :title, :type, :content_length
-
-  delegate :content_length, to: :object
 
   def type
     object.note_type
