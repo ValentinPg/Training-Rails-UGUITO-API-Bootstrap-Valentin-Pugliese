b6269f845d517978966b2b4037d953a0bc6774b7
Valentín Pugliese <105302328+ValentinPg@users.noreply.github.com>
2025-03-13 10:11:57 -0300

    feat:  [TR-734] books view in ActiveAdmin (#10)
    * feat: vista books active admin
    
    * fix: se agrega el user_id a los permited params
    
    ---------
    
    Co-authored-by: ValentinPg <valentinpugliese@outlook.com>

---
+++ app/admin/books.rb
@@ -0,0 +1,32 @@
+ActiveAdmin.register Book do
+  filter :title
+  filter :author
+  filter :genre
+  filter :utility
+
+  permit_params :utility_id, :genre, :author, :image, :title, :publisher, :year, :user_id
+
+  index do
+    selectable_column
+    id_column
+    column :title
+    column :author
+    column :genre
+    actions
+  end
+
+  form do |f|
+    f.inputs 'Book details', allow_destroy: true do
+      f.semantic_errors(*f.object.errors.keys)
+      f.input :title
+      f.input :author
+      f.input :genre
+      f.input :publisher
+      f.input :year
+      f.input :image, as: :url
+      f.input :utility
+      f.input :user, as: :select, collection: User.pluck(:email, :id)
+      f.actions
+    end
+  end
+end
